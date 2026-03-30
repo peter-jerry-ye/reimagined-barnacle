6323ddc6b728cf7a96efa9421bf482e9b7c25707
zihang <yezihang@idea.edu.cn>
2026-03-30 14:05:28 +0800

    feat: add initial moon formula for tap MVP

---
+++ Formula/moon.rb
@@ -0,0 +1,54 @@
+class Moon < Formula
+  desc "MoonBit build system and package manager"
+  homepage "https://www.moonbitlang.com"
+  url "https://cli.moonbitlang.com/install/unix.sh"
+  version "latest"
+  sha256 :no_check
+  license "AGPL-3.0-or-later"
+
+  def install
+    libexec.install "unix.sh" => "install-unix.sh"
+
+    moon_home = var/"moon"
+
+    # Bootstrap the toolchain using the official installer into Homebrew-managed var.
+    system({ "MOON_HOME" => moon_home.to_s }, "bash", libexec/"install-unix.sh", "latest")
+
+    wrappers = %w[
+      moon
+      moonc
+      moonrun
+      mooncake
+      moondoc
+      moonfmt
+      mooninfo
+      moon-ide
+      moon-lsp
+      moonbit-lsp
+      moon_cove_report
+    ]
+
+    wrappers.each do |name|
+      (bin/name).write <<~SH
+        #!/bin/bash
+        export MOON_HOME="${MOON_HOME:-#{moon_home}}"
+        exec "${MOON_HOME}/bin/#{name}" "$@"
+      SH
+      chmod 0755, bin/name
+    end
+  end
+
+  def caveats
+    <<~EOS
+      MoonBit toolchain files are installed into:
+        #{var}/moon
+
+      You can override this at runtime with:
+        export MOON_HOME=/path/to/another/moon-home
+    EOS
+  end
+
+  test do
+    assert_match "moon", shell_output("#{bin}/moon version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# reimagined-barnacle (temporary tap)
+
+Temporary Homebrew tap for MoonBit MVP.
+
+Install:
+
+```bash
+brew tap --custom-remote peter-jerry-ye/reimagined-barnacle git@github.com:peter-jerry-ye/reimagined-barnacle.git
+brew install peter-jerry-ye/reimagined-barnacle/moon
+```
